57e60d030702305e6f31e91e5ca55b8550c889b2
Aashrai Ravooru <ashrair@gmail.com>
2017-03-17 22:20:50 +0530

    Added delay in chart plotting and animation

--- data_munging.py
+++ data_munging.py
@@ -35,13 +35,26 @@ import datetime
 def default_factory():
 	return []
 date_dict = collections.defaultdict(default_factory)
+date_dict_weekend = collections.defaultdict(default_factory)
 for row in reader:
 	parsed_date = datetime.datetime.strptime(row['starttime'],"%m/%d/%Y %H:%M")
-	date_dict[parsed_date.date()].append(row)
+	if parsed_date.isoweekday() in range(1,6):
+		date_dict[parsed_date.date()].append(row)
+	else:
+		date_dict_weekend[parsed_date.date()].append(row)
 
-k = [k for k in date_dict.keys() if len(date_dict.get(k))==max([len(n) for n in date_dict.values()])]
+def getMaxKeyValue(dict):
+	return [k for k in dict.keys() if len(dict.get(k))==max([len(n) for n in dict.values()])][0]
+k_weekday = getMaxKeyValue(date_dict)
+k_weekend = getMaxKeyValue(date_dict_weekend)
+print k_weekday
 # print date_dict[k[0]]
-k = date_dict[k[0]]
-w = csv.DictWriter(open("trip_date_max.csv","w"),k[0].keys(),quoting=csv.QUOTE_ALL)
-w.writeheader()
-w.writerows(k)
+
+def createCsv(key, name, dict):
+	k = dict[key]
+	w = csv.DictWriter(open(name,"w"),k[0].keys(),quoting=csv.QUOTE_ALL)
+	w.writeheader()
+	w.writerows(k)
+
+createCsv(k_weekday,"trip_date_max.csv",date_dict);
+createCsv(k_weekend,"trip_date_max_weekend.csv",date_dict_weekend)
